10c411841335ec967554a7bdc27739b9b8219e51
Matheus Rodrigues <mr634580@gmail.com>
2021-06-30 22:44:52 -0300

    aplica serializacao dos dados

--- HotelIFBA/serializer.py
+++ HotelIFBA/serializer.py
@@ -1,33 +1,54 @@
 from django.db.models import fields
 from rest_framework import serializers
 from HotelIFBA.models import *
+from django.contrib.auth.models import User, Group
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta: 
+        model = User
+        fields = ['username', 'password']
+
+class GroupSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Group
+        fields = ['url', 'name']
 
 class EmpresaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Empresa
-        fields = ['nomeEmpresa', 'cnpj', 'proprietario', 'endereco', 'telefone', 'categoria', 'email']
+        fields = ['id', 'nomeEmpresa', 'proprietario', 'endereco', 'telefone', 'categoria', 'email']
 
 class ClienteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Cliente
-        fields = ['nomeCliente', 'nacionalidade', 'dtNascimento', 'endereco', 'telefone', 'idNumero', 'idData', 'email', 'senha']
+        fields = ['id', 'nomeCliente', 'nacionalidade', 'data_nascimento', 'endereco', 'telefone', 'numero_id', 'data_exp', 'email']
 
 class ColaboradorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Colaborador
-        fields = ['id', 'nomeCompleto', 'cpf', 'rg', 'ctps', 'cargo', 'admissao', 'jornadaDiaria', 'endereco']
+        fields = ['id', 'nomeCompleto', 'cpf', 'cargo', 'admissao', 'jornadaDiaria', 'endereco', 'login', 'senha']
 
 class ReservaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Reserva
-        fields = ['id', 'dataEntrada', 'dataSaida', 'tipoQuarto', 'qtdPessoas']
+        fields = ['id', 'dataEntrada', 'dataSaida', 'quarto', 'qtd_pessoas', 'servico', 'cliente']
 
 class EstadiaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Estadia
-        fields = ['id', 'cliente', 'quarto', 'dataEntrada', 'dataSaida', 'dadosPagamento']
+        fields = ['id', 'numero_cartao', 'cliente', 'quarto', 'dataEntrada', 'dataSaida', 'dadosPagamento', 'qtd_pessoas', 'qtd_quartos', 'reserva', 'mudanca_quarto']
 
 class QuartoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Quarto
-        fields = ['numeroQuarto', 'andar', 'categoria', 'interfoneNumero', 'capacidade']
+        fields = ['id', 'numeroQuarto', 'andar', 'categoria', 'interfoneNumero', 'capacidade', 'disponibilidade']
+
+class TipoServicoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TipoServico
+        fields = ['id', 'tipo', 'preco']
+
+class EstatisticaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Estatistica
+        fields = ['id','trimestre','taxaOcupacaoQuartos','taxaQuartosVendidos','faturamentoDoTrimestre','faturamentoAnual','ano','clientePremium']
