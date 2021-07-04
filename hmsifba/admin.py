9e96e2ed9f80ca39a1a741941c116caa0c81b29e
Matheus Rodrigues <mr634580@gmail.com>
2021-07-04 00:23:41 -0300

    corrige campos de registro e serializacao em estatistica

---
+++ hmsifba/admin.py
@@ -0,0 +1,47 @@
+from django.contrib import admin
+from hmsifba.models import *
+
+class Empresas(admin.ModelAdmin):
+    list_display = ('id', 'nomeEmpresa', 'proprietario', 'endereco', 'telefone', 'categoria', 'email')
+    list_display_links = ('id', 'nomeEmpresa', 'proprietario', 'endereco', 'telefone', 'categoria', 'email')
+
+class Clientes(admin.ModelAdmin):
+    list_display = ('id', 'nomeCliente', 'data_nascimento', 'email', 'endereco', 'nacionalidade', 'telefone',
+     'numero_id', 'data_exp')
+    list_display_links = ('id', 'nomeCliente', 'email')
+
+class Colaboradores(admin.ModelAdmin):
+    list_display = ('id', 'nomeCompleto', 'cpf', 'login', 'senha', 'cargo')
+    list_display_links = ('nomeCompleto', 'cargo', 'senha')
+
+class TipoServicos(admin.ModelAdmin):
+    list_display = ('id', 'preco', 'epoca_ano', 'tipo', 'qtd_pessoas')
+    list_display_links = ('id', 'preco', 'epoca_ano', 'tipo', 'qtd_pessoas')
+
+class Quartos(admin.ModelAdmin):
+    list_display = ('id', 'numeroQuarto', 'andar', 'capacidade', 'disponibilidade')
+    list_display_links = ('id', 'numeroQuarto', 'andar', 'capacidade', 'disponibilidade')
+
+class Reservas(admin.ModelAdmin):
+    list_display = ('id', 'dataEntrada', 'dataSaida', 'quarto', 'qtd_pessoas', 'cliente', 'servico')
+    list_display_links = ('id', 'dataEntrada', 'dataSaida', 'quarto', 'cliente')
+
+class Estadias(admin.ModelAdmin):
+    list_display = ('id', 'id', 'cliente', 'dataEntrada', 'dataSaida', 'qtd_pessoas', 'qtd_quartos', 'cliente', 'servico', 'dadosPagamento')
+    list_display_links = ('id', 'dataEntrada', 'dataSaida')
+
+class Estatisticas(admin.ModelAdmin):
+    list_display = ('id','semestre', 'taxaQuartosVendidos','faturamentoSemestre','faturamentoAnual','ano','clienteId', 'clientePremium','custoTotalCliente')
+    search_fields = ('id','semestre', 'taxaQuartosVendidos','faturamentoSemestre','faturamentoAnual','ano','clienteId', 'clientePremium','custoTotalCliente')
+
+
+
+admin.site.register(Empresa, Empresas)
+admin.site.register(Cliente, Clientes)
+admin.site.register(Colaborador, Colaboradores)
+admin.site.register(Reserva, Reservas)
+admin.site.register(Estadia, Estadias)
+admin.site.register(Quarto, Quartos)
+admin.site.register(TipoServico, TipoServicos)
+admin.site.register(DadosPagamento)
+admin.site.register(Estatistica, Estatisticas)
---
+++ hmsifba/serializer.py
@@ -0,0 +1,59 @@
+from django.db.models import fields
+from rest_framework import serializers
+from hmsifba.models import *
+from django.contrib.auth.models import User, Group
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta: 
+        model = User
+        fields = ['username', 'password']
+
+class GroupSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Group
+        fields = ['url', 'name']
+
+class EmpresaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Empresa
+        fields = ['id', 'nomeEmpresa', 'proprietario', 'endereco', 'telefone', 'categoria', 'email']
+
+class ClienteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cliente
+        fields = ['id', 'nomeCliente', 'nacionalidade', 'data_nascimento', 'endereco', 'telefone', 'numero_id', 'data_exp', 'email']
+
+class ColaboradorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Colaborador
+        fields = ['id', 'nomeCompleto', 'cpf', 'cargo', 'admissao', 'jornadaDiaria', 'login', 'senha']
+
+class ReservaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Reserva
+        fields = ['id', 'dataEntrada', 'dataSaida', 'quarto', 'qtd_pessoas', 'servico', 'cliente']
+
+class EstadiaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Estadia
+        fields = ['id', 'numero_cartao', 'cliente', 'quarto', 'dataEntrada', 'dataSaida', 'dadosPagamento', 'qtd_pessoas', 'qtd_quartos', 'reserva', 'mundanca_quarto']
+
+class QuartoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Quarto
+        fields = ['id', 'numeroQuarto', 'andar', 'categoria', 'interfoneNumero', 'capacidade', 'disponibilidade']
+
+class TipoServicoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TipoServico
+        fields = ['id', 'tipo', 'preco']
+
+class DadosPagamentoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DadosPagamento
+        fields = ['id', 'titular', 'agencia', 'conta', 'digito']
+
+class EstatisticaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Estatistica
+        fields = ['id','trimestre', 'taxaQuartosVendidos','faturamentoSemestre','faturamentoAnual','ano','clientePremium']
